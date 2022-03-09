fe6c478d7013dafde14349dc1bdc8b4b3f40ba09
chrislee1996 <prepchrislee@gmail.com>
2022-03-09 11:50:09 -0500

    adds layout and index views

--- server.js
+++ server.js
@@ -34,6 +34,31 @@ app.get('/', (req,res) => {
     res.send('your server is running')
 })
 
+app.get('/fruits/seed', (req, res)=> {
+    //arr of starter fruits
+    const startFruits = [
+        { name: "Orange", color: "orange", readyToEat: false },
+        { name: "Grape", color: "purple", readyToEat: false },
+        { name: "Banana", color: "orange", readyToEat: false },
+        { name: "Strawberry", color: "red", readyToEat: false },
+        { name: "Coconut", color: "brown", readyToEat: false }
+      ]
+
+    //when we seed data, there are a few steps involved
+    //delete all data that already exist ( will only happen if data exists)
+    Fruit.remove({})
+      .then(data=> {
+          console.log('this is what remove returns', data)
+          //then we create with our seed data
+          Fruit.create(startFruits)
+            .then(data => {
+                res.send(data)
+            })
+
+      })
+    //then we create with our seed data
+    //then we can send if we want to see that data
+})
 
 
 ////////////////////////////////////////
