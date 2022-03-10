9cad656fe87f4c708394769adcd28cd14478dd12
chrislee1996 <prepchrislee@gmail.com>
2022-03-10 15:35:06 -0500

    added mine page

--- controllers/user.js
+++ controllers/user.js
@@ -19,7 +19,6 @@ const router = express.Router()
 router.get('/signup', (req, res) => {
     res.render('users/signup')
 })
-
 // post to send the signup info
 router.post('/signup', async (req, res) => {
     // console.log('this is initial req.body in signup', req.body)
@@ -49,49 +48,48 @@ router.get('/login', (req, res) => {
 })
 // post to send the login info(and create a session)
 router.post('/login', async (req, res) => {
-    //first we need to get the data from the request body
+    console.log('request object', req)
+    // get the data from the request body
     const { username, password } = req.body
-    //then we search for the user
+    // then we search for the user
     User.findOne({ username })
         .then(async (user) => {
-    //check if the user exist 
-    if (user) {
-            //compare the password
-            //bcrypt.compare evaluates to a truthy or falsy value
-            const result = await  bcrypt.compare(password, user.password)
-            if (result) {
-                //then we need to use the session object
-                //store some properties in the session 
-                req.session.username= username
-                req.session.loggedIn= true      
-                //redirect to /fruits if login is successful
-                res.redirect('/fruits')
+            // check if the user exists
+            if (user) {
+                // compare the password
+                // bcrypt.compare evaluates to a truthy or a falsy value
+                const result = await bcrypt.compare(password, user.password)
+
+                if (result) {
+                    // then we'll need to use the session object
+                    // store some properties in the session
+                    req.session.username = username
+                    req.session.loggedIn = true
+                    // redirect to /fruits if login is successful
+                    res.redirect('/fruits')
+                } else {
+                    // send an error if the password doesnt match
+                    res.json({ error: 'username or password incorrect'})
+
+                }
             } else {
-                //send an error if password is wrong/doesnt match
-                res.json({error: 'username or password incorrect'})
+                // send an error if the user doesnt exist
+                res.json({ error: 'user does not exist' })
             }
-            
-    } else {
-            //send an error if user doesn't exists
-            res.json({error: 'username does not exist'})
-    }
-
-
-
-    //catch any other errors that occur 
         })
+        // catch any other errors that occur
         .catch(error => {
             console.log(error)
             res.json(error)
         })
 })
 
-// signout route -> destroy the session
-router.get('/logout', (req,res)=> {
-    //destroy the session and redirect to the main page
+// logout route -> destroy the session
+router.get('/logout', (req, res) => {
+    // destroy the session and redirect to the main page
     req.session.destroy(err => {
-        console.log(err)
-        res.redirect('/')
+        console.log('this is err in logout', err)
+        res.send('your session has been destroyed')
     })
 })
 
--- models/fruit.js
+++ models/fruit.js
@@ -1,29 +1,27 @@
 /////////////////////////////////
-//import dependencies
+// import dependencies
 /////////////////////////////////
 const mongoose = require('./connection')
 
 /////////////////////////////////
-//define fruits model
+// define our fruits model
 /////////////////////////////////
-//pull the schema and model constructors from mongoose
-//we're going to use something called  destructoring to accomplush this
-const {Schema, model} = mongoose
+// pull the schema and model constructors from mongoose
+// we're going to use something called destructuring to accomplish this
+const { Schema, model } = mongoose
 
-//make our fruits schema
-const fruitSchema = new Schema(
-    {
-        name: {type: String},
-        color: {type: String},
-        readyToEat: {type: Boolean},
-    },
-    {timestamps:true}
-)
+// make our fruits schema
+const fruitSchema = new Schema({
+    name: { type: String },
+    color: { type: String },
+    readyToEat: { type: Boolean },
+    username: { type: String }
+}, { timestamps: true })
 
-//make our fruit model
-const Fruit = model('Fruit', fruitSchema)
+// make our fruit model
+const Fruit = model("Fruit", fruitSchema)
 
 /////////////////////////////////
-//export our model
+// Export our Model
 /////////////////////////////////
 module.exports = Fruit
